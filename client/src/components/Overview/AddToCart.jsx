759a7728e1d506c3b7e9c59ac0a82d599579eefd
jpdavid <justinepdavid@gmail.com>
2021-02-26 15:07:38 -0800

    Created Add to Cart button, size, and quantity dropdown

--- client/src/components/Overview/AddToCart.jsx
+++ client/src/components/Overview/AddToCart.jsx
@@ -12,6 +12,14 @@ export default class AddToCart extends React.Component {
     return (
       <div>
         This is where the AddToCart goes
+        <select>
+          Choose Size
+          <option>--Sizes--</option>
+        </select>
+        <select>
+          Choose Quantity
+          <option>--Quantity--</option>
+        </select>
         <button type="button" id="cart ">Add to Cart</button>
       </div>
     )
