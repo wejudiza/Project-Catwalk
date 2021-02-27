23f1e8f2620e59d1e0f245562216fc2c94e8aae7
Yang Zhang <dirkzhang0104@live.com>
2021-02-26 16:28:30 -0800

    implemented backend functions to interact with database

--- server/controllers.js
+++ server/controllers.js
@@ -1,34 +1,138 @@
+const axios = require('axios');
+const gitToken = require('../config.js');
+
 const controllers = {
 
   // Products//
-  getAllProducts: () => {
-
+  getAllProducts: (req, res) => {
+    // console.log(req.body)
+    axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/products', {
+      headers: {
+        Authorization: gitToken.TOKEN,
+      },
+    })
+      .then((rawData) => {
+        res.status(200).send(rawData.data);
+      })
+      .catch((err) => {
+        res.status(400).send(err);
+      });
   },
-  getProductInfo: () => {
-
+  getProductInfo: (req, res) => {
+    axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/products/${req.params.product_id}`, {
+      headers: {
+        Authorization: gitToken.TOKEN,
+      },
+    })
+      .then((rawData) => {
+        res.status(200).send(rawData.data);
+      })
+      .catch((err) => {
+        res.status(400).send(err);
+      });
   },
-  getProductStyles: () => {
-
+  getProductStyles: (req, res) => {
+    axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/products/${req.params.product_id}/styles`, {
+      headers: {
+        Authorization: gitToken.TOKEN,
+      },
+    })
+      .then((rawData) => {
+        res.status(200).send(rawData.data);
+      })
+      .catch((err) => {
+        res.status(400).send(err);
+      });
   },
-  getRelatedProductIds: () => {
-
+  getRelatedProductIds: (req, res) => {
+    axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/products/${req.params.product_id}/related`, {
+      headers: {
+        Authorization: gitToken.TOKEN,
+      },
+    })
+      .then((rawData) => {
+        res.status(200).send(rawData.data);
+      })
+      .catch((err) => {
+        res.status(400).send(err);
+      });
   },
 
   // Reviews//
-  getAllReviews: () => {
-
+  getAllReviews: (req, res) => {
+    console.log(req.query)
+    axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/reviews/', {
+      headers: {
+        Authorization: gitToken.TOKEN,
+      },
+      params: {
+        page: req.query.page || 1,
+        count: req.query.count || 5,
+        sort: req.query.sort || '',
+        product_id: req.query.product_id,
+      },
+    })
+      .then((rawData) => {
+        res.status(200).send(rawData.data);
+      })
+      .catch((err) => {
+        res.status(400).send(err);
+      });
   },
-  getMetadataById: () => {
-
+  getMetadataById: (req, res) => {
+    axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/reviews/meta', {
+      headers: {
+        Authorization: gitToken.TOKEN,
+      },
+      params: {
+        product_id: req.params.product_id,
+      },
+    })
+      .then((rawData) => {
+        res.status(200).send(rawData.data);
+      })
+      .catch((err) => {
+        res.status(400).send(err);
+      });
   },
-  postReviewById: () => {
-
+  postReviewById: (req, res) => {
+    axios.post('https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/reviews/', req.body, {
+      headers: {
+        Authorization: gitToken.TOKEN,
+      },
+    })
+      .then((rawData) => {
+        res.status(200).send(rawData.data);
+      })
+      .catch((err) => {
+        res.status(400).send(err);
+      });
   },
-  markReviewAsHelpful: () => {
-
+  markReviewAsHelpful: (req, res) => {
+    axios.put(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/reviews/${Number(req.params.review_id)}/helpful`, null, {
+      headers: {
+        Authorization: gitToken.TOKEN,
+      },
+    })
+      .then(() => {
+        res.status(200).send('Thank you for your feedback');
+      })
+      .catch((err) => {
+        res.status(400).send(err);
+      });
   },
-  reportReview: () => {
-
+  reportReview: (req, res) => {
+    axios.put(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/reviews/${Number(req.params.review_id)}/report`, null, {
+      headers: {
+        Authorization: gitToken.TOKEN,
+      },
+    })
+      .then(() => {
+        res.status(200).send('Thank you for your report');
+      })
+      .catch((err) => {
+        res.status(400).send(err);
+      });
   },
 
   // Q&A//
@@ -58,16 +162,46 @@ const controllers = {
   },
 
   // Cart//
-  getCart: () => {
-
+  getCart: (req, res) => {
+    axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/cart', {
+      headers: {
+        Authorization: gitToken.TOKEN,
+      },
+    })
+      .then((rawData) => {
+        res.status(200).send(rawData.data);
+      })
+      .catch((err) => {
+        res.status(400).send(err);
+      });
   },
-  postProductToCartBySkuId: () => {
-
+  postProductToCartBySkuId: (req, res) => {
+    axios.post('https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/cart', req.body, {
+      headers: {
+        Authorization: gitToken.TOKEN,
+      },
+    })
+      .then((rawData) => {
+        res.status(200).send(rawData.data);
+      })
+      .catch((err) => {
+        res.status(400).send(err);
+      });
   },
 
   // Interactions//
-  postInteraction: () => {
-
+  postInteraction: (req, res) => {
+    axios.post('https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/interactions', req.body, {
+      headers: {
+        Authorization: gitToken.TOKEN,
+      },
+    })
+      .then((rawData) => {
+        res.status(200).send(rawData.data);
+      })
+      .catch((err) => {
+        res.status(400).send(err);
+      });
   },
 };
 
