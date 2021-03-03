6e2e7da155a61a06e736a77a2a683bfed53d6883
weillytong <weillydtong@gmail.com>
2021-03-03 15:13:33 -0800

    Added click funcitonality to add blank cards in the outfits list while still having the Add Outfits button

--- client/src/components/RelatedItems/OutfitsList.jsx
+++ client/src/components/RelatedItems/OutfitsList.jsx
@@ -6,26 +6,47 @@ export default class OutfitsList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      outfitsListId: [],
+      outfitsList: [],
     };
+    this.getProduct = this.getProduct.bind(this);
   }
 
-  // Map over a get request of the related items
+  getProduct() {
+    axios.get(`/api/products/${this.props.currentProduct}`)
+      .then((results) => {
+        console.log('results.data', results.data)
+        this.state.outfitsList.push(results.data)
+        this.setState({
+          outfitsList: this.state.outfitsList
+        }, () => {
+          console.log('this.state', this.state)
+        });
+      })
+      .catch((err) => console.log('getProduct err: ', err));
+  }
+
+
   render() {
     return (
       <div>
         <h4>
         Outfits List
         </h4>
-        <div className='list'>
-          {this.state.outfitsListId.length === 0 ?
-            <div className='card'>
-            </div>
+        <div>
+          {/* *** Include additional logic to check if that outfit ID has already been included in outfitsList */}
+          {this.state.outfitsList.length === 0 ?
+            <button className='card' id='outfitsBtn' onClick={this.getProduct}> "Click" to Add to Outfits </button>
             :
-            <div className='card'>
-              <Outfit />
-            <div className='card'>
-            </div>
+            <div className='list'>
+              {this.state.outfitsList.map((outfit, key) => {
+                return (
+                  <div className='card'>
+                    {console.log('outfit', outfit)}
+                    <Outfit outfit={outfit}/>
+                  </div>
+                )
+              })}
+              <button className='card' id='outfitsBtn' onClick={this.getProduct}> "Click" to Add to Outfits </button>
             </div>
           }
         </div>
