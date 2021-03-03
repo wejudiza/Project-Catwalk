d9ebb34615a62242487dc6a7ddee77952318f20b
jpdavid <justinepdavid@gmail.com>
2021-03-02 17:03:06 -0800

    Created conditional that tracks if the select dropdown is at 'select size'

--- client/src/components/Overview/AddToCart.jsx
+++ client/src/components/Overview/AddToCart.jsx
@@ -5,7 +5,7 @@ export default class AddToCart extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      size: '',
+      size: 'Select Size',
       quantity: 0,
       skus: [],
     };
@@ -14,11 +14,17 @@ export default class AddToCart extends React.Component {
   }
 
   handleSizeChange(e) {
-    const skuNum = e.target.options[e.target.selectedIndex].getAttribute('name');
-    this.setState({
-      size: e.target.value,
-      quantity: this.props.skus[skuNum].quantity,
-    });
+    if (e.target.value === 'Select Size') {
+      this.setState({
+        size: e.target.value,
+      })
+    } else {
+      const skuNum = e.target.options[e.target.selectedIndex].getAttribute('name');
+      this.setState({
+        size: e.target.value,
+        quantity: this.props.skus[skuNum].quantity,
+      }, () =>  console.log('cart state: ', this.state));
+    }
   }
 
   // onAddToCartClick() {
@@ -28,7 +34,7 @@ export default class AddToCart extends React.Component {
   selectedSizeMode() {
     var options = [...Array(16).keys()];
     options.shift();
-    if (this.state.size) {
+    if (this.state.size !== 'Select Size') {
       if (this.state.quantity < 15) {
         options = [...Array(this.state.quantity + 1).keys()];
         options.shift();
@@ -60,8 +66,8 @@ export default class AddToCart extends React.Component {
     return (
       <div>
         <br />
-        <select defaultValue={this.state.selectValue} onChange={this.handleSizeChange}>
-          <option value="default">Select Size</option>
+        <select onChange={this.handleSizeChange}>
+          <option value={this.value}>Select Size</option>
           {Object.keys(this.props.skus).map((sku, index) => (
             <option name={sku} key={index}>
               {this.props.skus[sku].size}
