1c16d7a7e343aa0177d176b8411a9a7a5445e5a6
weillytong <weillydtong@gmail.com>
2021-03-04 11:51:24 -0800

    Added left and right arrow buttons on Related Products list with no functionality + created an Arrow component to help with carousel functionality

--- client/src/components/RelatedItems/ProductsList.jsx
+++ client/src/components/RelatedItems/ProductsList.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import Product from './Product.jsx';
+import Arrow from './Arrow.jsx';
 
 export default class ProductsList extends React.Component {
   constructor(props) {
@@ -42,11 +43,17 @@ export default class ProductsList extends React.Component {
           Related Products
         </h4>
         <div className='list'>
+          <button className='fas fa-caret-left slide-arrow' >
+            <Arrow className='slide-arrow' direction='left'/>
+          </button>
           {this.state.productsListId.map((productId, key) => (
             <div className="card" key={key}>
               <Product productId={productId} currentProduct={this.props.currentProduct} getCurrentProductId={this.props.getCurrentProductId}/>
             </div>
           ))}
+          <button className='fas fa-caret-right slide-arrow'>
+            <Arrow className='slide-arrow' direction='right' />
+          </button>
         </div>
       </div>
     );
