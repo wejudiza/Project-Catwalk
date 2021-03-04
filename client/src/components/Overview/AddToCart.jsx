79ba352d13f6cdc2ab6be4fe997309c66a4d7bc5
jpdavid <justinepdavid@gmail.com>
2021-03-04 12:46:08 -0800

    Implemented Add to Cart functionality

--- client/src/components/Overview/AddToCart.jsx
+++ client/src/components/Overview/AddToCart.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
 // import axios from 'axios';
-import Select from 'react-select'
+import Select from 'react-select';
+import ReactModal from 'react-modal';
+
+const modalStyle = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+};
 
 export default class AddToCart extends React.Component {
   constructor(props) {
@@ -9,8 +21,13 @@ export default class AddToCart extends React.Component {
       size: 'Select Size',
       quantity: 0,
       skus: [],
+      selectedQ: 0,
+      showError: false,
+      showModal: false,
     };
     this.handleSizeChange = this.handleSizeChange.bind(this);
+    this.handleQuantityChange = this.handleQuantityChange.bind(this);
+    this.handleModal = this.handleModal.bind(this);
     this.selectedSizeMode = this.selectedSizeMode.bind(this);
     this.onAddToCartClick = this.onAddToCartClick.bind(this);
     // this.changedSkuMode = this.changedSkuMode.bind(this);
@@ -21,38 +38,57 @@ export default class AddToCart extends React.Component {
       this.setState({
         size: 'Select Size',
         quantity: 0,
-      })
+      });
     } else {
       this.setState({
         size: e.value,
         quantity: e.key,
+        showError: false,
       });
     }
   }
 
-  onAddToCartClick(e) {
+  handleQuantityChange(e) {
+    this.setState({
+      selectedQ: e.value,
+    });
+  }
+
+  handleModal() {
+    this.setState({
+      showModal: !this.state.showModal,
+    });
+  }
+
+  onAddToCartClick() {
+    console.log('clicked!')
     if (this.state.size === 'Select Size') {
-
+      this.setState({
+        showError: true,
+      });
     } else {
-
+      this.setState({
+        showModal: true,
+      });
     }
   }
 
   selectedSizeMode() {
-    var options = [...Array(16).keys()];
-    options.shift();
+    const numbers = [...Array(16).keys()];
+    numbers.shift();
     if (this.state.size !== 'Select Size') {
       if (this.state.quantity < 15) {
-        options = [...Array(this.state.quantity + 1).keys()];
-        options.shift();
+        numbers = [...Array(this.state.quantity + 1).keys()];
+        numbers.shift();
       }
-      const quantities = options.map((quantity) => (
+      const quantities = numbers.map((quantity) => (
         { value: quantity, label: quantity }
       ));
       return (
         <Select
           options={quantities}
-          value={quantities[0]}
+          // defaultValue={quantities[0]}
+          onChange={this.handleQuantityChange}
         />
       );
     }
@@ -79,21 +115,45 @@ export default class AddToCart extends React.Component {
 
   render() {
     const sizes = Object.keys(this.props.skus).map((sku, index) => (
-      {'value': this.props.skus[sku].size, 'label': this.props.skus[sku].size, 'key': this.props.skus[sku].quantity,}
-    ))
+      { value: this.props.skus[sku].size, label: this.props.skus[sku].size, key: this.props.skus[sku].quantity }
+    ));
     return (
       <div>
         <br />
+        {this.state.showError && <span style={{ color: 'red' }}>Please select size</span>}
         <Select
-        options={sizes}
-        placeholder={'Select Size'}
-        onChange={this.handleSizeChange}
-        style={{width: '50%'}} />
+          options={sizes}
+          placeholder="Select Size"
+          onChange={this.handleSizeChange}
+          style={{ width: '50%' }}
+        />
         {this.selectedSizeMode()}
         <br />
-        <button type="button" id="cart">Add to Cart</button>
+        <button type="button" id="cart" onClick={this.onAddToCartClick}>Add to Cart</button>
+        {this.state.showModal
+        && (
+        <ReactModal isOpen contentLabel="test" style={modalStyle} ariaHideApp={false} onRequestClose={this.handleModal}>
+          <p>
+            <b><u>Added to cart!</u></b>
+            <br />
+            <br />
+            Style:
+            {' '}
+            {this.props.currentStyle.name}
+            <br />
+            Size:
+            {' '}
+            {this.state.size}
+            <br />
+            Quantity:
+            {' '}
+            {this.state.selectedQ}
+          </p>
+          <button onClick={this.handleModal}>Close</button>
+        </ReactModal>
+        )}
       </div>
-    )
+    );
     // return (
     //   <div>
     //     <br />
