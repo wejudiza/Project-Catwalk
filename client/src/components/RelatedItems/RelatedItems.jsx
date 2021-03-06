d8fa3634a49dd2cb057e918f9a8a4c0ddde58deb
weillytong <weillydtong@gmail.com>
2021-03-06 15:36:11 -0800

    Got array of objects for related product info

--- client/src/components/RelatedItems/RelatedItems.jsx
+++ client/src/components/RelatedItems/RelatedItems.jsx
@@ -7,9 +7,52 @@ export default class RelatedItems extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      relatedIdArr: [],
+      relatedInfoArr: [],
+      relatedStylesArr: [],
+      relatedReviewsArr: [],
     };
+    this.getRelatedData = this.getRelatedData.bind(this);
   }
+
+  componentDidMount() {
+    this.getRelatedInfo(this.props.currentProduct)
+
+  }
+
+  // *** Do all axios requests here
+  getRelatedInfo(productId) {
+    axios.get(`api/products/${productId}/related`)
+      .then((relatedIdResults) => {
+        this.setState({
+          relatedIdArr: relatedIdResults.data
+        })
+      })
+      .then((relatedIdResults) => {
+        var relatedInfoResults = this.state.relatedIdArr.map((relatedId) => (
+          axios.get(`api/products/${relatedId}`)
+            .then((result) => {
+              return result.data
+            })
+        ));
+        Promise.all(relatedInfoResults)
+          .then((results) => {
+            this.setState({
+              relatedInfoArr: results
+            }, () => {
+              console.log(this.state)
+            })
+          })
+      })
+  }
+
+
+
+
+
+
+
+
 
   render() {
     return (
