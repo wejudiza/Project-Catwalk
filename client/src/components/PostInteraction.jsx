30ccf9a31c39859c711aac24caf824bb9385a9ae
Yang Zhang <dirkzhang0104@live.com>
2021-03-10 20:08:47 -0800

    implemented interaction post function with advanced react component

---
+++ client/src/components/PostInteraction.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import axios from 'axios';
+
+const postInteraction = (Component) => {
+  return class extends React.Component {
+    constructor (props) {
+      super(props);
+      this.state = {
+
+      }
+      this.postToApi = this.postToApi.bind(this);
+    }
+    postToApi(e, name) {
+      axios.post('/api/interactions', {
+        element: e.target.outerHTML,
+        widget: name,
+        time: new Date().toString() + 'FROM WEJUDIZA',
+      })
+      .then(() => {
+        // console.log(
+        //   {
+        //     element: e.target.outerHTML,
+        //     widget: name,
+        //     time: new Date().toString() + 'FROM WEJUDIZA',
+        //   }
+        // );
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    }
+
+    render() {
+      return (
+        <Component {...this.props} postToApi={this.postToApi}/>
+      )
+    }
+  }
+}
+
+export default postInteraction;
