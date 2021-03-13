cd2938f1580c54094073054212e5fe139d681127
Yang Zhang <dirkzhang0104@live.com>
2021-03-13 13:47:19 -0800

    removed unsed dependency

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const BrotliPlugin = require('brotli-webpack-plugin');
+// const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
   watch: true,
@@ -23,4 +25,19 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx'],
   },
+  plugins: [
+    new BrotliPlugin({ //brotli plugin
+      asset: '[path].br[query]',
+      test: /\.(js|css|html|svg)$/,
+      threshold: 10240,
+      minRatio: 0.8
+    })
+  //   new CompressionPlugin({
+  //   filename: '[path].gz[query]',
+  //   algorithm: 'gzip',
+  //   test: /\.(js|css|html|svg)$/,
+  //   threshold: 8192,
+  //   minRatio: 0.8
+  //   })
+  ],
 };
